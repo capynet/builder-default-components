1f91082e4b7380d684576ef32d58636f1c9ddda3
Marcelo Tosco <marcelo.tosco@1xinternet.de>
2023-10-21 23:23:36 +0200

    .

--- src/components/SearchInput/SearchInput.ce.vue
+++ src/components/SearchInput/SearchInput.ce.vue
@@ -7,13 +7,14 @@ const component = ref(null)
 
 interface Props {
   initialValue?: string
-  placeholder?: string
+  dataPlaceholder?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
   initialValue: '',
-  placeholder: '',
+  dataPlaceholder: 'Search',
 })
+
 
 const currentValue = ref(props.initialValue)
 
@@ -29,7 +30,13 @@ watch(currentValue, () => {
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
       </svg>
     </div>
-    <input v-model="currentValue" type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :placeholder="props.placeholder" required>
+    <input
+      v-model="currentValue"
+      type="search"
+      id="default-search"
+      class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      :placeholder="props.dataPlaceholder"
+    >
   </div>
 </template>
 
