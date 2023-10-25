0fe8c0d3989cd64fe989b962721f71a40631c7d9
Marcelo Tosco <marcelo.tosco@1xinternet.de>
2023-10-25 23:15:59 +0200

    .

--- src/components/facets/SearchFacetCheckbox/SearchFacetCheckbox.ce.vue
+++ src/components/facets/SearchFacetCheckbox/SearchFacetCheckbox.ce.vue
@@ -5,15 +5,13 @@ import {useEmit} from '../../../composables/useEmit'
 const {emitCustom} = useEmit()
 const component = ref(null)
 
-const props = defineProps<{
+defineProps<{
   conf: object
 }>()
 
 
 const submit = (e: Event, key: string, active: boolean) => {
-  const el = e.currentTarget as HTMLDivElement
-  console.log(el);
-  emitCustom("facet-change", el, {
+  emitCustom("facet-change", component, {
     key: key,
     active: !active,
     reset: false,
@@ -23,25 +21,25 @@ const submit = (e: Event, key: string, active: boolean) => {
 
 <template>
 
-<ul>
-  <li>
-    <div class="flex items-center mb-4" @click="submit($event, conf.key, conf.active)">
-      <input :checked="conf.active" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-      <label  class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ conf.label }} <span v-if="conf.showCount">{{ conf.count }}</span></label>
-    </div>
-  </li>
+  <ul ref="component">
+    <li>
+      <div class="flex items-center mb-4" @click="submit($event, conf.key, conf.active)">
+        <input :id="'chk' + conf.key" :checked="conf.active" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label :for="'chk' + conf.key" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ conf.label }} <span v-if="conf.showCount">{{ conf.count }}</span></label>
+      </div>
+    </li>
 
-  <li v-if="conf.results.length">
-    <ul class="pl-5 mt-2">
-      <li v-for="result in conf.results">
-        <div class="flex items-center mb-4" @click="submit($event, result.key, result.active)">
-          <input :checked="result.active"  type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-          <label class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ result.label }} <span v-if="result.showCount">{{ result.count }}</span></label>
-        </div>
-      </li>
-    </ul>
-  </li>
-</ul>
+    <li v-if="conf.results.length">
+      <ul class="pl-5 mt-2">
+        <li v-for="result in conf.results">
+          <div class="flex items-center mb-4" @click="submit($event, result.key, result.active)">
+            <input :id="'chk' + result.key" :checked="result.active" type="checkbox" :value="result.key" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+            <label :for="'chk' + result.key" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ result.label }} <span v-if="result.showCount">{{ result.count }}</span></label>
+          </div>
+        </li>
+      </ul>
+    </li>
+  </ul>
 
 
 </template>
